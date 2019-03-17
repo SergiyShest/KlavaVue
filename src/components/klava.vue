5f8eeca4504adefd177cd79cc3202e4171e60119
Sergey <Shest_akow@rambler.ru>
2019-03-17 20:36:14 +0300

    тмз

---
+++ src/components/klava.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+   <input  v-on:onkeypress="inputNextChar"  v: v-model="input"/>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+
+export default {
+  //@Prop() private msg!: string;
+
+ data(){return {
+   input:'tast'  
+ };
+ },
+ methods:{
+     inputNextChar( ev: any){
+    alert(ev);
+     }
+ }
+}
+</script>
