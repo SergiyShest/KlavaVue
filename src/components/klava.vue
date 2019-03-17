2a45f43d53e4d265d54517da547187fe8e658c7b
Sergey <Shest_akow@rambler.ru>
2019-03-17 20:37:48 +0300

    temporary

--- src/components/klava.vue
+++ src/components/klava.vue
@@ -1,23 +1,62 @@
 <template>
   <div class="about">
-   <input  v-on:onkeypress="inputNextChar"  v: v-model="input"/>
+    <div >{{Example}}</div>
+   <div  >
+   <span   v-for="item in res"   v-bind:class="{ 'gr': item.ok ,'re': item.ok===false}"> {{ item.text }} {{item.ok }}</span>
+  </div>
+   <input    v: v-model="input"/>
   </div>
 </template>
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-
-
+<script >
+import Vue from "vue";
 export default {
-  //@Prop() private msg!: string;
+  name: "Klava",
+  props: {
+    Example: String
+  },
 
  data(){return {
+   res:[{text:'',ok:true},{text:'',ok:false},{text:'',ok:null}],
    input:'tast'  
  };
  },
  methods:{
-     inputNextChar( ev: any){
-    alert(ev);
+       compare(){
+     var textOk='';
+     var erText='';
+     var textAfteEr='';
+     var notInptText='';    
+  for(i=0;i<Example.length;i++)
+    { 
+      var inpChar='';
+      if(input.length<i){inpChar=input[i];}
+      
+      exChar=Example[i];
+      if(inpChar==exChar && erText.length==0)
+      {
+        textOk+=inpChar;continue;
+      }
+       if(inpChar=='')
+      {
+        notInptText+=inpChar;continue;
+      } 
+      if(inpChar!=exChar||erText.length>0)
+      {
+        erText+=inpChar;
+      } 
+      if(inpChar!=exChar||erText.length==0)
+      {
+        textOk+=inpChar;
+      }      
+    }   
+     
      }
- }
+ },
+ watch:{
+input:function(newV, oldV ){
+ 
+}
+}
 }
 </script>
+
---
+++ src/components/klavaInp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <span class="gr">{{textOk}}</span>
+    <span class="re">{{erText}}</span>
+    <span>{{notInpText}}</span> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputedString",
+  props: {
+    Inputed: String,
+    Example: String
+  },
+  data:{textOk:'',erText:'',notInptText:''}
+
+ ,
+  methods:{
+         compare(){
+   textOk='';erText='';notInptText='';
+  for(i=0;i<Example.length;i++)
+    { 
+      var inpChar='';
+      if(Inputed.length<i){inpChar=Inputed[i];}
+      
+      exChar=Example[i];
+      if(inpChar==exChar && erText.length==0)
+      {
+        textOk+=inpChar;continue;
+      }
+       if(inpChar=='')
+      {
+        notInptText+=inpChar;continue;
+      } 
+      if(inpChar!=exChar||erText.length>0)
+      {
+        erText+=inpChar;
+      } 
+    }   
+     
+     }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.gr{
+color: #42b983;
+background-color: rgb(212, 212, 236);
+}
+.re{
+color: #bb1212;
+}
+</style>
